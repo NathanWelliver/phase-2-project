f10f2bb70a2be32fda08e6d75660ad6ea55624a6
Nathan Welliver <nwelliver.564@gmail.com>
2024-06-13 17:45:20 -0400

    Project set-up still on going, add NavBar and routes

---
+++ src/Components/WorkoutCard.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function WorkoutCard({workout}) {
+    return (
+        <article>
+            <h2>{workout.name}</h2>
+            <h4>{workout.reps}</h4>
+            <h4>{workout.body}</h4>
+        </article>
+    )
+}
+
+export default WorkoutCard;
---
+++ src/Pages/Form.js
@@ -0,0 +1,11 @@
+
+
+function Form(){
+    return (
+        <main>
+            <h1>Form component</h1>
+        </main>
+    )
+}
+
+export default Form;
