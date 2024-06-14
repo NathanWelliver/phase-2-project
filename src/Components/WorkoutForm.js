429dde33645c8d827d81b0a867551e7ee311bae9
Nathan Welliver <nwelliver.564@gmail.com>
2024-06-14 15:29:19 -0400

    Workout form has beed designed, and has functionality, meaning it can submit data to the json server

--- src/Components/WorkoutForm.js
+++ src/Components/WorkoutForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
