afa38e530c458e01c5bbaee16a95ef3a5d6e83d8
Nathan Welliver <nwelliver.564@gmail.com>
2024-06-14 12:21:02 -0400

    Added info to restday

---
+++ src/Pages/RestDay.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import NavBar from "../Components/NavBar";
+
+function RestDay(){
+    const [rest, setRest] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:3000/rest")
+        .then(r => r.json())
+        .then(data => setRest(data))
+        .catch(error => console.log(error))
+    }, [])
+
+    return (
+        <>
+        <header>
+            <NavBar />
+        </header>
+        <main>
+            <h1>Remember To Take a Rest!</h1>
+            <h4>
+                Rest days are important! You may not realize that, or want to continue to work out 
+                even if you already did you workouts for the week or beginning of the week.
+                but if you dont rest your body cant recover fully. Remember, Keys to proper recovery
+                after any good workout is rest or sleep, food, and some extra activities that you 
+                normally wouldn't think would help. But ice baths can help muscle's recover faster.
+                Don't over train! Your body has limits, try to stay in them to help prevent injury's.
+            </h4>
+            <h5>
+            You should also stay hydrated after a workout. The American College of Sports
+            Medicine recommends drinking 16 to 24 ounces of water 2 to 3 hours before
+            working out, 4 to 8 ounces every 15 to 20 minutes during your workout, and 16 to 24
+            ounces for every pound of fluid you lose after your workout. If you're
+            exercising for more than 60 minutes, you can also try a sports drink with
+            electrolytes and carbohydrates to replenish what your body has used. 
+            </h5>
+            <h3>Foods to help recovery:</h3>
+            <h5>
+                Turkey sandwiches
+                Pasta dishes
+                Rice bowls with vegetables and beans or chicken
+                A banana and low-fat chocolate milk (full-fat milk may be harder to digest after a workout)
+                Whole-grain crackers and peanut butter
+                A smoothie with yogurt and frozen berries
+                Find the food combinations that make you feel best and enjoy!
+            </h5>
+        </main>
+        </>
+    )
+}
+
+export default RestDay;
