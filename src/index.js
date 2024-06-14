a4e938cd792858454bea81897c12ffcbbab0363f
Nathan Welliver <nwelliver.564@gmail.com>
2024-06-14 13:00:50 -0400

    update

--- src/index.js
+++ src/index.js
@@ -2,14 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import routes from "./routes";
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import routes from "./routes";
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const router = createBrowserRouter(routes);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={router} />);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<RouterProvider router={router} />);
