47af42acc812f6801b99299a7388df439f25dbc6
Nathan Welliver <nwelliver.564@gmail.com>
2024-06-14 12:22:45 -0400

    redesigned the functionality of this page and its components, not done

---
+++ src/Components/Form.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+function Form(){
+    return (
+        <main>
+            <h1>Form component</h1>
+        </main>
+    )
+}
+
+export default Form;
--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,7 +1,12 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import NavBar from "../Components/NavBar";
+
 function About() {
     return (
       <>
         <main>
+          <NavBar />
           <h1>This is my about component!</h1>
         </main>
       </>
