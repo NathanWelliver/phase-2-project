430c866abc171fa63bed43e0d9c13985dedc0003
Nathan Welliver <nwelliver.564@gmail.com>
2024-06-14 15:26:39 -0400

    Added a working date, and recommended workout

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,19 +1,14 @@
 import NavBar from "../Components/NavBar"
 import WorkoutOfDay from "../Components/WorkoutOfDay"
-import Date from "../Components/Date"
+import DateComponent from "../Components/DateComponent"
 import React from "react";
 import {useState, useEffect} from "react"
 
 function Home(){
-    const [days, setDays] = useState([]);
+    const [date, setDate] = useState(new Date());
+    const [workout, setWorkout] = useState('');
+    const [bodyPart, setBodyPart] = useState('');
     const [workouts, setWorkouts] = useState([]);
-
-    useEffect(() => {
-        fetch("http://localhost:3000/days")
-        .then(r => r.json())
-        .then(data => setDays(data))
-        .catch(error => console.log(error))
-    }, [])
 
     useEffect(() => {
         fetch("http://localhost:3000/workouts")
@@ -21,6 +16,27 @@ function Home(){
         .then(data => setWorkouts(data))
         .catch(error => console.log(error))
     }, [])
+
+    useEffect(() => {
+        const day = date.getDay();
+        let recommendedBodyPart = '';
+        if (day === 1 || day === 4) {
+            recommendedBodyPart = "Lower Body";
+        } else if (day === 2 || day === 5){
+            recommendedBodyPart = "Upper Body";
+        } else {
+            recommendedBodyPart = "Rest Day";
+        }
+
+        setBodyPart(recommendedBodyPart);
+
+        const filteredWorkouts = workouts.filter(workout => workout.body === recommendedBodyPart);
+        const randomWorkout = filteredWorkouts[Math.floor(Math.random() * filteredWorkouts.length)];
+
+        if (randomWorkout){
+            setWorkout(randomWorkout.name + '(' + randomWorkout.reps + ')');
+        }
+    }, [date, workouts]);
 
     return (
         <>
@@ -30,10 +46,13 @@ function Home(){
             <main>
                 <h1>Welcome!</h1>
                     <h2>
-                        Today's Date: <Date />
+                        Today is: <DateComponent date={date}/>
                     </h2>
                 <h3>
-                    Excercise To Try: <WorkoutOfDay />
+                    <WorkoutOfDay 
+                        bodyPart={bodyPart}
+                        workout={workout}
+                    />
                 </h3>
             </main>
         </>
