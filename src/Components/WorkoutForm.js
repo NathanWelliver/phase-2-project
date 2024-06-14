506ebf9f8551f2d485e414fa95cc31a4157581ec
Nathan Welliver <nwelliver.564@gmail.com>
2024-06-14 14:01:47 -0400

    Completed the functions for the about page and form componenet

---
+++ src/Components/WorkoutForm.js
@@ -0,0 +1,79 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
+function WorkoutForm({setWorkouts}){
+    const [formData, setFormData] = useState({
+        name: "",
+        reps: "",
+        body: ""
+    })
+
+    function handleChange(e){
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    function handleSubmit(e){
+        e.preventDefault();
+
+        const newWorkout = {
+            name: formData.name,
+            reps: formData.reps,
+            body: formData.body
+        };
+
+        fetch("http://localhost:3000/workouts", {
+            method: "Post",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newWorkout)
+        })
+        .then(r => r.json())
+        .then(addedWorkout => {
+            setWorkouts((workouts) => [...workouts, addedWorkout])
+        });
+    };
+
+    return (
+        <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="workout-name">
+                <Form.Label>Workout Name</Form.Label>
+                <Form.Control 
+                    type="text" 
+                    name="name" 
+                    placeholder="Enter workout name" 
+                    onChange={handleChange}/>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="workout-reps">
+                <Form.Label>Reps</Form.Label>
+                <Form.Control 
+                    type="text" 
+                    placeholder="Enter # of sets x reps"
+                    name="reps"
+                    onChange={handleChange}
+                    />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="workout-body">
+                <Form.Label>Body Part</Form.Label>
+                <Form.Control 
+                    type="text" 
+                    placeholder="Enter target body part" 
+                    name="body"
+                    onChange={handleChange}
+                    />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+        </Form>
+    )
+}
+
+export default WorkoutForm;
--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,13 +1,38 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import NavBar from "../Components/NavBar";
+import WorkoutCard from "../Components/WorkoutCard";
+import { Accordion } from "react-bootstrap";
+import WorkoutForm from "../Components/WorkoutForm";
 
 function About() {
+  const [workouts, setWorkouts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/workouts")
+    .then(r => r.json())
+    .then(data => setWorkouts(data))
+    .catch(error => console.log(error))
+}, [])
+
+const workoutList = workouts.map(workout => <WorkoutCard key={workout.id} workout={workout}/>)
+
     return (
       <>
+      <header>
+        <NavBar />
+      </header>
         <main>
-          <NavBar />
-          <h1>This is my about component!</h1>
+          <Accordion>
+            <Accordion.Item eventKey="0">
+              <Accordion.Header>Add a new Workout!</Accordion.Header>
+              <Accordion.Body>
+                <WorkoutForm setWorkouts={setWorkouts}/>
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
+          <h1>Workouts:</h1>
+          {workoutList}
         </main>
       </>
     );
