ceb42d1be0a4125ae8d42a935e859292716ff22f
Nathan Welliver <nwelliver.564@gmail.com>
2024-06-27 22:20:27 -0400

    added outlet to pass data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
 import NavBar from "./Components/NavBar";
 
 function App(){
+    const [workouts, setWorkouts] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/workouts")
+        .then(r => r.json())
+        .then(data => setWorkouts(data))
+        .catch(error => console.log(error))
+    }, [])
+    
+
     return  (
         <div className="app">
             <header>
                 <NavBar />
             </header>
+            <Outlet context={{workouts: workouts, setWorkouts: setWorkouts}}/>
         </div>
     )
 }
