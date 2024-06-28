aa98c09d00c77b8794125a35eb323c7e1ee187ec
Nathan Welliver <nwelliver.564@gmail.com>
2024-06-27 22:25:47 -0400

    passes data from app to home through outletcontext

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,21 +1,26 @@
-import NavBar from "../Components/NavBar"
-import WorkoutOfDay from "../Components/WorkoutOfDay"
-import DateComponent from "../Components/DateComponent"
+import NavBar from "../Components/NavBar";
+import WorkoutOfDay from "../Components/WorkoutOfDay";
+import DateComponent from "../Components/DateComponent";
 import React from "react";
-import {useState, useEffect} from "react"
+import {useState, useEffect} from "react";
+import { Outlet, useOutletContext } from "react-router-dom";
+
+/* 
+Rename components for whats in them,
+make the workout form a controlled componenet,
+put state for workout in the main app.js file and pass them down, 
+only one fetch request should be made,
+pass the data down through outlet context.
+remove date state and assign date to a javascript variable.
+look into making a seperate callback function for the workoutform sumbission/ update state.
+*/
 
 function Home(){
-    const [date, setDate] = useState(new Date());
     const [workout, setWorkout] = useState('');
     const [bodyPart, setBodyPart] = useState('');
-    const [workouts, setWorkouts] = useState([]);
+    const {workouts} = useOutletContext();
 
-    useEffect(() => {
-        fetch("http://localhost:3000/workouts")
-        .then(r => r.json())
-        .then(data => setWorkouts(data))
-        .catch(error => console.log(error))
-    }, [])
+    const date = new Date();
 
     useEffect(() => {
         const day = date.getDay();
@@ -40,9 +45,6 @@ function Home(){
 
     return (
         <div className="home">
-            <header>
-                <NavBar />
-            </header>
             <main>
                 <h1 className="welcome-header">Welcome!</h1>
                     <h2 className="today">
