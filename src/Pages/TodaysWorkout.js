fc5125c0c96dae001bee57572f62709d817daeca
Nathan Welliver <nwelliver.564@gmail.com>
2024-07-08 13:52:49 -0400

    Removed comments

--- src/Pages/TodaysWorkout.js
+++ src/Pages/TodaysWorkout.js
@@ -2,16 +2,6 @@ import React from "react";
 import {useState, useEffect} from "react";
 import { useOutletContext } from "react-router-dom";
 
-/* 
-Rename components for whats in them,
-make the workout form a controlled componenet,
-put state for workout in the main app.js file and pass them down, 
-only one fetch request should be made,
-pass the data down through outlet context.
-remove date state and assign date to a javascript variable.
-look into making a seperate callback function for the workoutform sumbission/ update state.
-*/
-
 function Home(){
     const [workout, setWorkout] = useState('');
     const [bodyPart, setBodyPart] = useState('');
